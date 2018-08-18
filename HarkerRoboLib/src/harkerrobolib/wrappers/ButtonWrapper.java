66b55fb9294217af7d9f4399dd2c5c366c0a8f6a
FinnitoProductions <finn@teachmy.com>
2018-08-17 17:47:41 -0700

    Updated file system

---
+++ HarkerRoboLib/src/harkerrobolib/wrappers/ButtonWrapper.java
@@ -0,0 +1,31 @@
+package harkerrobolib.wrappers;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.command.Command;
+import harkerrobolib.commands.*;
+
+
+/**
+ * Wrapper class for Button that adds cancelWhenReleased and alters whilePressed to be more intuitive
+ *
+ * @author Manan
+ */
+abstract class ButtonWrapper extends Button {
+
+    /**
+     * Cancels the command when the button is released
+     * @param c the command to cancel
+     */
+    public void cancelWhenReleased(Command c) {
+	    whenReleased(new CancelCommand(c));
+    }
+
+    /**
+     * Starts command when button is pressed, and cancels it when button is released
+     * @param c the command to run while pressed
+     */
+    public void whilePressed(Command c) {
+        whenPressed(c);
+        cancelWhenReleased(c);
+    }
+}
