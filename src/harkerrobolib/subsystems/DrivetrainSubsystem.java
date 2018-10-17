f87eceb39aa89c232dee74812a67b4d738fbefc7
Finn Frankis <finn@teachmy.com>
2018-10-16 20:51:58 -0700

    Document DrivetrainSubsystem and modify neutral mode application

--- src/harkerrobolib/subsystems/DrivetrainSubsystem.java
+++ src/harkerrobolib/subsystems/DrivetrainSubsystem.java
@@ -6,12 +6,14 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import harkerrobolib.wrappers.TalonSRXWrapper;
 
 /**
- * Represents a general Drivetrain subsystem.
+ * Represents a general Drivetrain subsystem with two master motor controllers and two optional follower controllers.
  * @author Finn Frankis
  * @version Aug 17, 2018
  */
@@ -22,6 +24,14 @@ public abstract class DrivetrainSubsystem extends Subsystem {
     private IMotorController rightFollower;
     private boolean hasFollowers;
 
+    /**
+     * Constructs a new Drivetrain subsystem with two master controllers and two follower controllers.
+     * 
+     * @param leftMaster the left master Talon.
+     * @param rightMaster the right master Talon.
+     * @param leftFollower the left follower motor controller (Talon or Victor).
+     * @param rightFollower the right folllower motor controller (Talon or Victor).
+     */
     public DrivetrainSubsystem (TalonSRXWrapper leftMaster, TalonSRXWrapper rightMaster, IMotorController leftFollower,
             IMotorController rightFollower) {
         this.leftMaster = leftMaster;
@@ -33,25 +43,43 @@ public abstract class DrivetrainSubsystem extends Subsystem {
         hasFollowers = true;
     }
 
+    /**
+     * Constructs a new Drivetrain subsystem with two master controllers.
+     * 
+     * @param leftMaster the left master Talon.
+     * @param rightMaster the right master Talon.
+     */
     public DrivetrainSubsystem (TalonSRXWrapper leftMaster, TalonSRXWrapper rightMaster) {
         this(leftMaster, rightMaster, null, null);
         hasFollowers = false;
     }
 
-    
+    /**
+     * Applies a given operation to both master Talons (see lambdas in Java for more clarification).
+     * @param consumer the operation to be applied.
+     */
     public void applyToMasters (Consumer<TalonSRXWrapper> consumer) {
         consumer.accept(leftMaster);
         consumer.accept(rightMaster);
     }
 
-
+    /**
+     * Applies a given operation to both master and follower motor controllers.
+     * @param consumer the operation to be applied.
+     */
     public void applyToAll (Consumer<IMotorController> consumer) {
         consumer.accept(leftMaster);
         consumer.accept(rightMaster);
-        consumer.accept(leftFollower);
-        consumer.accept(rightFollower);
+        
+        if (hasFollowers) {
+	        consumer.accept(leftFollower);
+	        consumer.accept(rightFollower);
+        }
     }
 
+    /**
+     * Tells the follower controllers to follow the master controllers.
+     */
     public void followMasters () {
         if (hasFollowers) {
             leftFollower.follow(leftMaster);
@@ -59,12 +87,22 @@ public abstract class DrivetrainSubsystem extends Subsystem {
         }
     }
     
+    /**
+     * Sets all active controllers to a given neutral mode (brake or coast).
+     * @param nm the neutral mode to be applied.
+     */
     public void setNeutralMode (NeutralMode nm) {
-        if (hasFollowers) applyToAll ((talon) -> {talon.setNeutralMode(nm);});
-        else applyToMasters((talon) -> {talon.setNeutralMode(nm);});
+        applyToAll((controller) -> {controller.setNeutralMode(nm);});
 
     }
 
+    /**
+     * Configures both masters for current limiting.
+     * @param peakLimit the peak limit (temporary, to account for current spikes).
+     * @param peakTime the time for which the peak limit is active
+     * @param continuousLimit the continuous limit, or the one which is active after the conclusion of the peak limit.
+     * @param timeout the time after which a failed current limit command will stop being retried.
+     */
     public void setCurrentLimit (int peakLimit, int peakTime, int continuousLimit, int timeout) {
         Consumer<TalonSRXWrapper> currentLimit = (talon) -> {
             int newTimeout = (timeout == -1) ? talon.getDefaultTimeout() : timeout;
@@ -75,30 +113,60 @@ public abstract class DrivetrainSubsystem extends Subsystem {
         applyToMasters (currentLimit);
     }
     
+    /**
+     * Configures both masters for current limiting, where each one's default timeout is used.
+     * @param peakLimit the peak limit (temporary, to account for current spikes).
+     * @param peakTime the time for which the peak limit is active
+     * @param continuousLimit the continuous limit, or the one which is active after the conclusion of the peak limit.
+     */
     public void setCurrentLimit (int peakLimit, int peakTime, int continuousLimit) {
         setCurrentLimit(peakLimit, peakTime, continuousLimit, -1);
     }
     
+    /**
+     * Sets both masters to a given output.
+     * @param mode the mode to which both Talons should be set.
+     * @param outputValue the value of the output tied to the given control mode.
+     */
     public void setBoth (ControlMode mode, double outputValue) {
     	applyToMasters((talon) -> {talon.set(mode, outputValue);});
     }
     
+    /**
+     * Resets both masters.
+     */
     public void resetMasters() {
     	applyToMasters ((talon) -> {talon.reset();});
     }
     
+    /**
+     * Gets the left master controller.
+     * @return the left master controller.
+     */
     public TalonSRXWrapper getLeftMaster() {
     	return leftMaster;
     }
     
+    /**
+     * Gets the right master controller.
+     * @return the right master controller.
+     */
     public TalonSRXWrapper getRightMaster() {
     	return rightMaster;
     }
     
+    /**
+     * Gets the left follower controller.
+     * @return the left follower controller.
+     */
     public IMotorController getLeftFollower() {
     	return leftFollower;
     }
     
+    /**
+     * Gets the right follower controller.
+     * @return the right follower controller.
+     */
     public IMotorController getRightFollower() {
     	return rightFollower;
     }
