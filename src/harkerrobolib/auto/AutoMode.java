2cca29f3c991a22f0c0443dce0a062e4e9f39ef9
Finn Frankis <finn@teachmy.com>
2018-10-18 15:01:21 +0100

    Break AutoMode into left, center, and right command groups

--- src/harkerrobolib/auto/AutoMode.java
+++ src/harkerrobolib/auto/AutoMode.java
@@ -3,13 +3,17 @@ package harkerrobolib.auto;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public abstract class AutoMode extends CommandGroup {
-	public AutoMode() {
-		addCommands();
-	}
-	
 	public enum StartLocation {
 		LEFT, CENTER, RIGHT
 	}
+
+	public AutoMode(StartLocation loc) {
+		if (loc == StartLocation.LEFT) {addSequential(getLeftCommands());}
+		else if (loc == StartLocation.CENTER) {addSequential (getCenterCommands());}
+		else if (loc == StartLocation.RIGHT) {addSequential(getRightCommands());} 
+	}
 	
-	public abstract void addCommands();
+	public abstract CommandGroup getLeftCommands();
+	public abstract CommandGroup getCenterCommands();
+	public abstract CommandGroup getRightCommands();
 }
