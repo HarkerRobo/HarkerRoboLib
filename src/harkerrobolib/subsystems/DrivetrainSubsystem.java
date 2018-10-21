b838daf8eddee7a97acbdc7fcf95c656497c8115
Finn Frankis <finn@teachmy.com>
2018-10-21 22:44:35 +0100

    Add useful debugging methods to DrivetrainSubsystem

--- src/harkerrobolib/subsystems/DrivetrainSubsystem.java
+++ src/harkerrobolib/subsystems/DrivetrainSubsystem.java
@@ -3,6 +3,7 @@ package harkerrobolib.subsystems;
 import java.util.function.Consumer;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -10,6 +11,7 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import harkerrobolib.wrappers.TalonSRXWrapper;
 
 /**
@@ -169,5 +171,59 @@ public abstract class DrivetrainSubsystem extends Subsystem {
      */
     public IMotorController getRightFollower() {
     	return rightFollower;
+    }
+    
+    /**
+     * Configures both Talons to point to a given sensor.
+     * @param fd the type of sensor to which the Talon should use
+     * @param pidLoop the loop index where this sensor should be placed [0,1]
+     */
+    public void configBothFeedbackSensors(FeedbackDevice fd, int pidLoop)
+    {
+        getLeftMaster().configSelectedFeedbackSensor(fd, 
+                    pidLoop);
+        getRightMaster().configSelectedFeedbackSensor(fd, 
+                    pidLoop);
+    }
+    
+    /**
+     * Prints the current output percentage to the motors to SmartDashboard.
+     */
+    public void printMotorOutputPercentage()
+    {
+        SmartDashboard.putNumber("Left Talon Output Percentage", getLeftMaster().getMotorOutputPercent());
+        SmartDashboard.putNumber("Right Talon Output Percentage", getRightMaster().getMotorOutputPercent());
+    }
+    
+    /**
+     * Prints the closed loop error of the Talons in a given loop.
+     * @param pidLoop the loop index [0,1]
+     */
+    public void printClosedLoopError (int pidLoop)
+    {
+        SmartDashboard.putNumber("Left Talon Closed Loop Error " + (pidLoop == 0 ? "Primary" : "Auxiliary"), getLeftMaster().getClosedLoopError(pidLoop));
+        SmartDashboard.putNumber("Right Talon Closed Loop Error " + (pidLoop == 0 ? "Primary" : "Auxiliary"), getRightMaster().getClosedLoopError(pidLoop));
+    }
+    
+    /**
+     * Prints the sensor positions of the Talons in a given loop.
+     * @param pidLoop the loop index [0,1]
+     */
+    public void printSensorPositions (int pidLoop)
+    {
+        SmartDashboard.putNumber("Left Talon Position " + (pidLoop == 0 ? "Primary" : "Auxiliary"), getLeftMaster().getSelectedSensorPosition(pidLoop));
+        SmartDashboard.putNumber("Right Talon Position" + (pidLoop == 0 ? "Primary" : "Auxiliary"), getRightMaster().getSelectedSensorPosition(pidLoop));
+    }
+    
+    /**
+     * Determines whether the closed loop error for both sides is within a given value.
+     * @param loopIndex the loop index, either primary or auxiliary [0,1]
+     * @param allowableError the error tolerance to be checked
+     * @return true if the absolute value of the error is within the value; false otherwise
+     */
+    public boolean isClosedLoopErrorWithin (int loopIndex, double allowableError)
+    {
+        return Math.abs(getLeftMaster().getClosedLoopError(loopIndex)) < allowableError
+                && Math.abs(getRightMaster().getClosedLoopError(loopIndex)) < allowableError;
     }
 }
