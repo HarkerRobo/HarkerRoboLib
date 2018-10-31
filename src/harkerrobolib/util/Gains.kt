67c82d379d4aae8005a79c49155458c18768e697
Finn Frankis <finn@teachmy.com>
2018-10-30 18:32:36 -0700

    Optimize Gains class for Kotlin syntax

--- src/harkerrobolib/util/Gains.kt
+++ src/harkerrobolib/util/Gains.kt
@@ -5,24 +5,4 @@ package harkerrobolib.util
  * @author Finn Frankis
  * @version 10/21/18
  */
-class Gains @JvmOverloads constructor(private val kF: Double, private val kP: Double, private val kI: Double, private val kD: Double, val iZone: Double = 0.0) {
-
-    constructor(kP: Double, kI: Double, kD: Double) : this(0.0, kP, kI, kD, 0.0) {}
-
-
-    fun getkF(): Double {
-        return kF
-    }
-
-    fun getkP(): Double {
-        return kP
-    }
-
-    fun getkI(): Double {
-        return kI
-    }
-
-    fun getkD(): Double {
-        return kD
-    }
-}
+class Gains (val kF: Double = 0.0, val kP: Double = 0.0, val kI: Double = 0.0, val kD: Double = 0.0, val iZone: Double = 0.0)
