7b0bbcf29862b83a1d44979a513d3349e950085a
Angela Jia <42385351+22angiej@users.noreply.github.com>
2018-10-31 16:57:22 -0700

    Create ArmSubsystem

---
+++ src/harkerrobolib/subsystems/ArmSubsystem.kt
@@ -0,0 +1,44 @@
+package harkerrobolib.subsystems
+
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.DemandType
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import edu.wpi.first.wpilibj.Talon
+import edu.wpi.first.wpilibj.command.Subsystem
+import harkerrobolib.wrappers.TalonSRXWrapper
+import jdk.nashorn.internal.objects.Global
+
+/**
+ *
+ * @version 10/31/18
+ */
+abstract class ArmSubsystem(val talon : TalonSRXWrapper, val feedForwardGrav : Double = 0.0 ) : Subsystem(){
+
+    enum class ArmDirection {
+        UP, DOWN
+    }
+
+    fun setCurrentLimits( peakTime : Int, peakCurrent : Int, contCurrent : Int, timeout : Int = talon.defaultTimeout ) {
+        with (talon) {
+            configPeakCurrentDuration(peakTime, timeout)
+            configPeakCurrentLimit(peakCurrent, timeout)
+            configContinuousCurrentLimit(contCurrent, timeout)
+            enableCurrentLimit(true)
+        }
+    }
+
+    fun armMotionPercentOutput(output: Double, direction: ArmDirection) {
+        val modifiedOutput = output//MathUtil.constrain(output, MAX_MOTION_SPEED, MIN_MOTION_SPEED)
+        if (direction == ArmDirection.UP) {
+            armMotionPercentOutput(modifiedOutput)
+        } else {
+            armMotionPercentOutput(-modifiedOutput)
+        }
+    }
+
+    fun armMotionPercentOutput (output: Double) {
+        talon.set(ControlMode.PercentOutput, output, DemandType.ArbitraryFeedForward, feedForwardGrav)
+    }
+
+    fun getTalonCurrent() = talon.outputCurrent
+}
