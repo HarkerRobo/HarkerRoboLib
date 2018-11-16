6e1b728e4b8c71ed2bd706c63b169d1ad9fce57d
FinnitoProductions <finn@teachmy.com>
2018-11-15 17:21:54 -0800

    Convert additional util/commands to Java

--- src/harkerrobolib/commands/Print.kt
+++
@@ -1,9 +0,0 @@
-package harkerrobolib.commands
-
-/**
- * Prints a given value to the console.
- * @param value the value to be printed
- * @author Finn Frankis
- * @version 10/17/18
- */
-class Print (value: String) : CallMethod({ println(value) })
--- src/harkerrobolib/util/Gains.kt
+++
@@ -1,8 +0,0 @@
-package harkerrobolib.util
-
-/**
- * Designed to store PID constants in an easily accessible way.
- * @author Finn Frankis
- * @version 10/21/18
- */
-data class Gains (val kF: Double = 0.0, val kP: Double = 0.0, val kI: Double = 0.0, val kD: Double = 0.0, val iZone: Double = 0.0)
