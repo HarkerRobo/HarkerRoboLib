056de28726409f914065e019d69f638ffbb723a8
FinnitoProductions <finn@teachmy.com>
2018-11-18 17:26:51 -0800

    Add previous methods back to MathUtil

--- src/harkerrobolib/util/MathUtil.java
+++ src/harkerrobolib/util/MathUtil.java
@@ -1,8 +1,15 @@
 package harkerrobolib.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import jaci.pathfinder.Waypoint;
+
 /**
  * Contains a series of useful mathematical functions.
  * @author Finn Frankis
+ * @author Angela Jia
  * @version 10/16/18
  */
 public final class MathUtil {
@@ -44,4 +51,32 @@ public final class MathUtil {
 		if (Math.abs(inputValue) <= deadband) {return 0;}
 		return inputValue > 0 ? map (inputValue, deadband, 1, 0, 1) : map(inputValue, -1, -deadband, -1, 0);
 	}
+	
+	 /**
+     *  Reflects array of waypoints across the horizontal line at the specified y value without changing
+     *  the initial array.
+     *
+     *  @param  waypoints   waypoints used for modification
+     *  @param  yVal        y value across which points will be reflected
+     *  @return reflected array of waypoints
+     */
+    public List<Waypoint> reflectY(List<Waypoint> waypoints, double yVal) {
+        waypoints = new ArrayList<Waypoint>(waypoints);
+        waypoints.replaceAll(waypoint -> new Waypoint(waypoint.x, 2 * yVal - waypoint.y, - waypoint.angle));
+        return waypoints;
+    }
+
+    /**
+     * Calculates setpoint of the robot's left and right side for turning in place
+     * based on the desired angle and width of the robot.
+     *
+     * @param   angleDegrees    desired angle in degrees to turn the robot
+     * @param   robotWidth      width of the robot in feet
+     *
+     * @return  array of the left and right setpoint in feet
+     */
+    public List<Double> turnInPlace(double angleDegrees, double robotWidth) {
+        double angleRadians = angleDegrees * Math.PI/180;
+        return Arrays.asList(new Double[] {angleRadians*robotWidth, -angleRadians*robotWidth});
+    }
 }
