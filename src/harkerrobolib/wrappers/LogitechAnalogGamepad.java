3f851bb792762adb95cf47e8cf52b3ded0af40b1
20JatinK <20finnf@students.harker.org>
2019-03-29 17:50:42 -0700

    Modify LogitechAnalogGamepad button bindings

--- src/harkerrobolib/wrappers/LogitechAnalogGamepad.java
+++ src/harkerrobolib/wrappers/LogitechAnalogGamepad.java
@@ -11,18 +11,18 @@ public class LogitechAnalogGamepad extends HSGamepad {
     public static final int B = 2;
     public static final int X = 3;
     public static final int Y = 4;
-    public static final int SELECT = 9;
-    public static final int START = 10;
+    public static final int SELECT = 7;
+    public static final int START = 8;
 
-    public static final int STICK_LEFT = 11;
-    public static final int STICK_RIGHT = 12;
+    public static final int STICK_LEFT = 9;
+    public static final int STICK_RIGHT = 10;
 
     public static final int BUMPER_LEFT = 5;
     public static final int BUMPER_RIGHT = 6;
     
     public static final int LEFT_X = 0;
     public static final int LEFT_Y = 1;
-    public static final int RIGHT_X = 2;
+    public static final int RIGHT_X = 4;
     public static final int RIGHT_Y = 5;
     public static final int TRIGGER_LEFT = 2;
     public static final int TRIGGER_RIGHT = 3;
