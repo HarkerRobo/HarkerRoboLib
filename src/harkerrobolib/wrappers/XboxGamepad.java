6885734bf42e1802b174d27171773e83b66ec3e4
Jatin <20JatinK@Students.harker.org>
2020-02-29 22:02:11 -0800

    Add TriggerButton functionality to XboxGamepad

--- src/harkerrobolib/wrappers/XboxGamepad.java
+++ src/harkerrobolib/wrappers/XboxGamepad.java
@@ -25,6 +25,9 @@ public class XboxGamepad extends HSGamepad {
     public static final int RIGHT_Y = 5;
     public static final int LEFT_TRIGGER = 2;
     public static final int RIGHT_TRIGGER = 3;
+
+    private HSTriggerButton leftTrigger;
+    private HSTriggerButton rightTrigger;
     
 	public XboxGamepad(int port) {
 		super(port, 
@@ -32,8 +35,19 @@ public class XboxGamepad extends HSGamepad {
 				START, SELECT, 
 				STICK_LEFT, STICK_RIGHT, 
 				BUMPER_LEFT, BUMPER_RIGHT,
-				LEFT_X, LEFT_Y, RIGHT_X, RIGHT_Y);
-	}
+                LEFT_X, LEFT_Y, RIGHT_X, RIGHT_Y);
+                
+        leftTrigger = new HSTriggerButton(this, LEFT_TRIGGER);
+        rightTrigger = new HSTriggerButton(this, RIGHT_TRIGGER);
+    }
+    
+    public HSTriggerButton getButtonTriggerLeft() {
+        return leftTrigger;
+    }
+
+    public HSTriggerButton getButtonTriggerRight() {
+        return rightTrigger;
+    }
 	
     public double getRightTrigger() {
     	return getRawAxis(RIGHT_TRIGGER);
