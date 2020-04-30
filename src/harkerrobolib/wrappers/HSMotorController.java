bf9dc4f195142586638cd094a3620481091e7787
Ada <23adap@students.harker.org>
2020-04-29 22:26:19 -0700

    add methods to HSMotorController

--- src/harkerrobolib/wrappers/HSMotorController.java
+++ src/harkerrobolib/wrappers/HSMotorController.java
@@ -1,5 +1,45 @@
 package harkerrobolib.wrappers;
 
+import com.ctre.phoenix.ErrorCode;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.VelocityMeasPeriod;
+
 public interface HSMotorController {
+
+    ErrorCode setStatusFramePeriod(StatusFrameEnhanced frame, int periodMs, int timeoutMs);
+
+    ErrorCode setStatusFramePeriod(StatusFrameEnhanced frame, int periodMs);
+
+    int getStatusFramePeriod(StatusFrameEnhanced frame, int timeoutMs);
+
+    int getStatusFramePeriod(StatusFrameEnhanced frame);
+
+    double getOutputCurrent();
+
+    double getStatorCurrent();
+
+    double getSupplyCurrent();
+
+    ErrorCode configVelocityMeasurementPeriod(VelocityMeasPeriod period, int timeoutMs);
+
+    ErrorCode configVelocityMeasurementPeriod(VelocityMeasPeriod period);
+
+    ErrorCode configVelocityMeasurementWindow(int windowSize, int timeoutMs);
+
+    ErrorCode configVelocityMeasurementWindow(int windowSize);
+
+    ErrorCode configForwardLimitSwitchSource(LimitSwitchSource type, LimitSwitchNormal normalOpenOrClose, int timeoutMs);
+
+    ErrorCode configForwardLimitSwitchSource(LimitSwitchSource type, LimitSwitchNormal normalOpenOrClose);
+
+    ErrorCode configReverseLimitSwitchSource(LimitSwitchSource type, LimitSwitchNormal normalOpenOrClose, int timeoutMs);
+
+    ErrorCode configReverseLimitSwitchSource(LimitSwitchSource type, LimitSwitchNormal normalOpenOrClose);
+
+    int isFwdLimitSwitchClosed();
+
+    int isRevLimitSwitchClosed();
 
 }
